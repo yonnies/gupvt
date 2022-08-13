70156651ab9ff10386f4183b1987e61aa7475e87
Yoana Yakova <yoanayakova@Yoanas-MacBook-Air.local>
2022-08-13 14:23:26 +0300

    Heroku setup

--- db-connect.php
+++ db-connect.php
@@ -1,19 +1,19 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "gupap";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "gupap";
 
-// Create connection
+// // Create connection
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-//header('Content-Type: text/html; charset=utf-8');
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// //header('Content-Type: text/html; charset=utf-8');
 
-$conn->set_charset("utf8"); 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+// $conn->set_charset("utf8"); 
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
 
 //Get Heroku ClearDB connection information.
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
@@ -24,6 +24,13 @@ $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+//$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+$conn->set_charset("utf8"); 
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 ?>
