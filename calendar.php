cbd82878efb185e97909f4eb7c1aab9801ea1627
Yoana Yakova <48910349+yonnies@users.noreply.github.com>
2023-09-22 16:27:06 +0300

    add comments to calendar.php

--- calendar.php
+++ calendar.php
@@ -1,24 +1,40 @@
+<!-- 
+	This page displays a calendar using PHP code and includes necessary templates
+    	The calendar rendering code is modularized in 'cal_code.php' for better organization.
+-->
 <?php 
-    $add_jquery = TRUE; 
-    include('templates/header.php');    
-    include_once('cal_code.php'); 
-    include('templates/navbar.php'); 
+	// Set a flag to include jQuery
+	$add_jquery = TRUE; 
+	
+	// Include the header template
+	include('templates/header.php');    
+	
+	// Include the calendar rendering code
+	include_once('cal_code.php'); 
+	
+	// Include the navigation bar template
+	include('templates/navbar.php'); 
 ?>
 
 
 
 <div class="calendar_frame" style="max-width:1600px">
-      <div class="row">
-      <div class="col-12">
-      <div>
-        <h2 style="text-align: center;">Календар</h2>
-        <div id="calendar_div">
-	            <?php echo getCalendar(); ?>
-        </div>
-      </div>
-    </div>
-  </div>
+	<div class="row">
+      	<div class="col-12">
+      	<div>
+        	<h2 style="text-align: center;">Календар</h2>
+
+		<!-- Display the calendar by calling the getCalendar() function -->
+		<div id="calendar_div">
+	        	<?php echo getCalendar(); ?>
+        	</div>
+      	</div>
+    	</div>
+  	</div>
 </div>
  
- <?php include('templates/footer.php') ?>
+<?php 
+	// Include the footer template
+	include('templates/footer.php') 
+?>
 </html>
