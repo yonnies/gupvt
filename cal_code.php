1cacffc06a099a5bf4e52494d3c5b5432c80354a
Yoana Yakova <48910349+yonnies@users.noreply.github.com>
2023-09-22 17:10:03 +0300

    Add comments to cal_code.php

--- cal_code.php
+++ cal_code.php
@@ -1,7 +1,18 @@
+<!-- 
+	Full implementation of the calendar functionality
+	Code adapted from:
+ 	https://www.spaceotechnologies.com/blog/php-event-calendar-jquery-ajax/
+
+	Modifications made by Yoana Yakova:
+	 - Solved various implementation bugs to ensure smoother functionality.
+	 - Redesigned the calendar interface for a customized look and feel.
+	 - Enhanced event display by embedding event lists within each date cell.
+	 - Implemented dynamic pop-up text boxes to provide detailed event information.
+-->
+
+
 <?php
-/*
- * Function requested by Ajax
- */
+// Defining the functions that can be requested by Ajax
 if(isset($_POST['func']) && !empty($_POST['func'])){
 	switch($_POST['func']){
 		case 'getCalendar':
@@ -15,19 +26,20 @@ if(isset($_POST['func']) && !empty($_POST['func'])){
 	}
 }
  
-/*
- * Get calendar full HTML
- */
+
+// Display calendar HTML
+
 function getCalendar($year = '',$month = '')
 {
+	// Defining day/month/year variables used in code
 	$dateYear = ($year != '')?$year:date("Y");
 	$dateMonth = ($month != '')?$month:date("m");
 	$date = $dateYear.'-'.$dateMonth.'-01';
 	$currentMonthFirstDay = date("N",strtotime($date));
 
+	// Calculate the number of days in a given month
 	function days_in_month($month, $year)
 	{
-		// calculate number of days in a month
 		return $month == 2 ? ($year % 4 ? 28 : ($year % 100 ? 29 : ($year % 400 ? 28 : 29))) : (($month - 1) % 7 % 2 ? 30 : 31);
 	}
 	$totalDaysOfMonth = days_in_month($dateMonth,$dateYear);
@@ -37,197 +49,179 @@ function getCalendar($year = '',$month = '')
 	$boxDisplay = ($totalDaysOfMonthDisplay <= 36)?35:42;
 ?>
 	
-	<div id="calendar_section" >
-
-	<?php
-	
-	
-	?>
-		<h2>
-			<a href="javascript:void(0);" onclick = "changeCalendar('calendar_div', '<?php echo date("Y", strtotime($date.' - 1 Month')); ?>' , '<?php echo date("m",strtotime($date.' - 1 Month')); ?>');">&lt;&lt;</a>
+	<div id="calendar_section">
+	<!-- Calendar header, navigation, and event list -->
+	<h2>
+		<a href="javascript:void(0);" onclick = "changeCalendar('calendar_div', '<?php echo date("Y", strtotime($date.' - 1 Month')); ?>' , '<?php echo date("m",strtotime($date.' - 1 Month')); ?>');">&lt;&lt;</a>
             
-			<select name="month_dropdown" class="month_dropdown dropdown"><?php echo getAllMonths($dateMonth); ?></select>
-			<select name="year_dropdown" class="year_dropdown dropdown"><?php echo getYearList($dateYear); ?></select>
+		<select name="month_dropdown" class="month_dropdown dropdown"><?php echo getAllMonths($dateMonth); ?></select>
+		<select name="year_dropdown" class="year_dropdown dropdown"><?php echo getYearList($dateYear); ?></select>
 			
-			<span>  </span>
-			<a href="javascript:void(0);"    onclick=  "changeCalendar('calendar_div','<?php echo date("Y", strtotime($date.' + 1 Month')); ?>','<?php echo date("m",strtotime($date.' + 1 Month')); ?>');">&gt;&gt;</a>
+		<span>  </span>
+		<a href="javascript:void(0);"    onclick=  "changeCalendar('calendar_div','<?php echo date("Y", strtotime($date.' + 1 Month')); ?>','<?php echo date("m",strtotime($date.' + 1 Month')); ?>');">&gt;&gt;</a>
         </h2>
 
-		<div id="event_list" class="none"></div>
-		<div id="calendar_section_top">
-			<ul>
-				
-				<li>Пон</li>
-				<li>Вт</li>
-				<li>Ср</li>
-				<li>Чет</li>
-				<li>Пет</li>
-				<li>Съб</li>
-				<li>Нед</li>
-
-			</ul>
-		</div>
-		<div id="calendar_section_bot">
-			<ul>
-			<?php 
-				$dayCount = 1; 
-				for($cb=1;$cb<=$boxDisplay;$cb++){
-					if(($cb >= $currentMonthFirstDay || $currentMonthFirstDay == 1) && $cb < ($totalDaysOfMonthDisplay)){
-						//Current date
-						$currentDate = $dateYear.'-'.$dateMonth.'-'.$dayCount;
-						$eventNum = 0;
-						//Include db configuration file
-						include( 'db-connect.php');
-						//Get number of events based on the current date
-						
-						$result = $conn->query("SELECT title, date, time, place,link FROM events WHERE date = '".$currentDate."' ");				
-						$eventNum = $result->num_rows;
-						
-						
-						//Make current date blue and echo it
-						
-						if(strtotime($currentDate) == strtotime(date("Y-m-d"))){
-							echo '<li date="'.$currentDate.'" class="date_cell">';
-							echo '<ul>';
-						    echo '<div ><div class="date-colour"> '.$dayCount.'</div>';
-						}else{
-							echo '<li date="'.$currentDate.'" class="date_cell">';
-							echo '<ul>';
-						    echo '<div>'.$dayCount;
-						}
-						
-						
-						
-						//Date cell
-						if($eventNum > 0){
+	<!-- Display a list of all events if any-->
+	<div id="event_list" class="none"></div>
 	
-							$eventListHTML = '';
-							if($result->num_rows > 0){
-								$i=0;
-								while($row = $result->fetch_assoc()){
+	<!-- Days of the week headers -->
+	<div id="calendar_section_top">
+	<ul>
+		<li>Пон</li>
+		<li>Вт</li>
+		<li>Ср</li>
+		<li>Чет</li>
+		<li>Пет</li>
+		<li>Съб</li>
+		<li>Нед</li>
 
-									$time = strtotime(htmlspecialchars($row['date']));
-									$myFormatForView = date("d/m/y", $time);
-									$eventListHTML .= 
-									'<div id="'.$i.'" date="'.$currentDate.'" class="event-list">
-										<div class="event-frame">'.$row['title'].'</div>
-
-									<div id="date_popup_'.$currentDate.''.$i.'" class="date_popup_wrap">
-									<p class="popup_event_title"><b><i>'.htmlspecialchars($row['title']).'</i></b></p>
-                        			<p class="popup_event"><b>Дата: </b>'.$myFormatForView.'<b> &nbsp;&nbsp;  Час: </b>'.substr(htmlspecialchars($row['time']),0,5).'</p>
-									<p class="popup_event"><b>Място: </b>'.htmlspecialchars($row['place']).'</p>
-									<a target="_blank" href="'.$row['link'].'"><p class="popup_event_link">Линк към facebook събитието</p></a>
-
-									</div>
-									</div>';
-									
-									$i++;
-									
-								}
-								
-							}
-							echo $eventListHTML;
-						}
-
-						echo '</div></li></ul>';
-					
-						
-						echo '';
-						
-						
-						
-						$dayCount++;
-			?>
-			<?php }else{ ?>
-				<li><span>&nbsp;</span></li>
-			<?php } } ?>
-			</ul>
-		</div>
+	</ul>
 	</div>
 	
+	<!-- Main calendar grid-->
+	<div id="calendar_section_bot">
+		<ul>
+		<?php 
+		$dayCount = 1; 
+		for($cb=1;$cb<=$boxDisplay;$cb++){
+			if(($cb >= $currentMonthFirstDay || $currentMonthFirstDay == 1) && $cb < ($totalDaysOfMonthDisplay)){
+				// Current date
+				$currentDate = $dateYear.'-'.$dateMonth.'-'.$dayCount;
+				$eventNum = 0;
+				
+				// Include db configuration file
+				include( 'db-connect.php');
+				
+				// Get the number of events based on the current date
+				$result = $conn->query("SELECT title, date, time, place,link FROM events WHERE date = '".$currentDate."' ");				
+				$eventNum = $result->num_rows;
 
-
-
-
-
-
- 
-	<script type="text/javascript">
-
-
-
-		function changeCalendar(target_div,year,month){
-			$.ajax({
-				type:'POST',
-				url:'cal_code.php',
-				data:'func=getCalendar&year='+year+'&month='+month,
-				success:function(html){
-					$('#'+target_div).html(html);
+				// Change the current date colour
+				if(strtotime($currentDate) == strtotime(date("Y-m-d"))){
+					echo '<li date="'.$currentDate.'" class="date_cell">';
+					echo '<ul>';
+				    echo '<div ><div class="date-colour"> '.$dayCount.'</div>';
+				}else{
+					echo '<li date="'.$currentDate.'" class="date_cell">';
+					echo '<ul>';
+				    echo '<div>'.$dayCount;
 				}
 				
-			});
-		}
-		
-		function getEvents(date){
-			$.ajax({
-				type:'POST',
-				url:'cal_code.php',
-				data:'func=getEvents&date='+date,
-				success:function(html){
-					$('#event_list').html(html);
-					$('#event_list').slideDown('slow');
+				// Check if there are any events for a given date
+				if($eventNum > 0){
+
+					$eventListHTML = '';
+					if($result->num_rows > 0){
+						$i=0;
+						while($row = $result->fetch_assoc()){
+							// Event details
+							$time = strtotime(htmlspecialchars($row['date']));
+							$myFormatForView = date("d/m/y", $time);
+							$eventListHTML .= 
+							'<div id="'.$i.'" date="'.$currentDate.'" class="event-list">
+								<div class="event-frame">'.$row['title'].'</div>
+
+							<div id="date_popup_'.$currentDate.''.$i.'" class="date_popup_wrap">
+							<p class="popup_event_title"><b><i>'.htmlspecialchars($row['title']).'</i></b></p>
+							<p class="popup_event"><b>Дата: </b>'.$myFormatForView.'<b> &nbsp;&nbsp;  Час: </b>'.substr(htmlspecialchars($row['time']),0,5).'</p>
+							<p class="popup_event"><b>Място: </b>'.htmlspecialchars($row['place']).'</p>
+							<a target="_blank" href="'.$row['link'].'"><p class="popup_event_link">Линк към facebook събитието</p></a>
+
+							</div>
+							</div>';
+							
+							$i++;
+						}
+					}
+					echo $eventListHTML;
 				}
-			});
-		}
-		
-		function addEvent(date){
-			$.ajax({
-				type:'POST',
-				url:'cal_code.php',
-				data:'func=addEvent&date='+date,
-				success:function(html){
-					$('#event_list').html(html);
-					$('#event_list').slideDown('slow');
-				}
-			});
-		}
-		
-		$(document).ready(function(){
+				echo '</div></li></ul>';
+				echo '';
+				
+				$dayCount++;
+		?>
+		<?php }else{ ?>
+			<li><span>&nbsp;</span></li>
+		<?php } } ?>
+		</ul>
+	</div>
+</div>
 	
+
+<script type="text/javascript">
+	// JavaScript functions for handling calendar interactions
+	function changeCalendar(target_div,year,month){
+		$.ajax({
+			type:'POST',
+			url:'cal_code.php',
+			data:'func=getCalendar&year='+year+'&month='+month,
+			success:function(html){
+				$('#'+target_div).html(html);
+			}
 			
-
-			$('.event-list').click(function(){
-				date = $(this).attr('date');
-				i = $(this).attr('id');
-				//$(".date_popup_wrap").fadeOut();
-				$("#date_popup_"+date+i).fadeIn();	
-			});
-
-			$('.event-list').click(function(){
-				var $this = $(this);
-				if ($this.hasClass("clicked-once")) {
-					// already been clicked once, hide it
-					$(".date_popup_wrap").fadeOut();
-					$this.removeClass("clicked-once");	
-				}
-				else {
-					// first time this is clicked, mark it
-					$this.addClass("clicked-once");
-				}
-					
-			});
-
-
-			$('.month_dropdown').on('change',function(){
-				changeCalendar('calendar_div',$('.year_dropdown').val(),$('.month_dropdown').val());
-			});
-			$('.year_dropdown').on('change',function(){
-				changeCalendar('calendar_div',$('.year_dropdown').val(),$('.month_dropdown').val());
-			});
-			$(document).click(function(){
-				$('#event_list').slideUp('slow');
-			});
 		});
+	}
+	
+	function getEvents(date){
+		$.ajax({
+			type:'POST',
+			url:'cal_code.php',
+			data:'func=getEvents&date='+date,
+			success:function(html){
+				$('#event_list').html(html);
+				$('#event_list').slideDown('slow');
+			}
+		});
+	}
+	
+	function addEvent(date){
+		$.ajax({
+			type:'POST',
+			url:'cal_code.php',
+			data:'func=addEvent&date='+date,
+			success:function(html){
+				$('#event_list').html(html);
+				$('#event_list').slideDown('slow');
+			}
+		});
+	}
+	
+	$(document).ready(function(){
+
+		
+
+		$('.event-list').click(function(){
+			date = $(this).attr('date');
+			i = $(this).attr('id');
+			//$(".date_popup_wrap").fadeOut();
+			$("#date_popup_"+date+i).fadeIn();	
+		});
+
+		$('.event-list').click(function(){
+			var $this = $(this);
+			if ($this.hasClass("clicked-once")) {
+				// already been clicked once, hide it
+				$(".date_popup_wrap").fadeOut();
+				$this.removeClass("clicked-once");	
+			}
+			else {
+				// first time this is clicked, mark it
+				$this.addClass("clicked-once");
+			}
+				
+		});
+
+
+		$('.month_dropdown').on('change',function(){
+			changeCalendar('calendar_div',$('.year_dropdown').val(),$('.month_dropdown').val());
+		});
+		$('.year_dropdown').on('change',function(){
+			changeCalendar('calendar_div',$('.year_dropdown').val(),$('.month_dropdown').val());
+		});
+		$(document).click(function(){
+			$('#event_list').slideUp('slow');
+		});
+	});
 	</script>
 <?php
 }
